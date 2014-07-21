f518ccb40597703bf09d1935b2ff6c1635dff44b
Stephen Tu <stephen@qadium.com>
2014-07-21 13:21:21 -0700

    fix broken test
    hopefully, travis CI builds successfully now

--- test/test_cxx_imports.py
+++ test/test_cxx_imports.py
@@ -1,6 +1,21 @@
-def test_import():
-    from microscopes.cxx.mixture.model import state, fixed_state, bind, bind_fixed
-    assert state and fixed_state and bind and bind_fixed
-    from microscopes.cxx.models import bb, nich
-    s = state(10, [bb, nich])
-    assert s
+def test_cxx_import():
+    from microscopes.cxx.mixture.model import \
+        state, fixed_state, \
+        bind, bind_fixed, \
+        initialize, initialize_fixed, \
+        deserialize, deserialize_fixed
+    assert state and fixed_state
+    assert bind and bind_fixed
+    assert initialize and initialize_fixed
+    assert deserialize and deserialize_fixed
+
+def test_py_import():
+    from microscopes.py.mixture.model import \
+        state, \
+        bind, \
+        initialize, \
+        deserialize
+    assert state
+    assert bind
+    assert initialize
+    assert deserialize
