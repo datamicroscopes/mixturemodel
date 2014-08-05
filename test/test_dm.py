f2077b923a16e94a1f85f515fe1984c5a6474c51
Stephen Tu <stephentu@csail.mit.edu>
2014-08-05 13:29:39 -0700

    better test cases for DM model

--- test/test_dm.py
+++ test/test_dm.py
@@ -11,26 +11,10 @@ from microscopes.cxx.common.recarray.dataview import numpy_dataview as cxx_numpy
 from microscopes.cxx.common.rng import rng
 
 import numpy as np
+import itertools as it
 
-from nose.tools import assert_almost_equals, assert_sequence_equal
+from nose.tools import assert_almost_equals, assert_sequence_equal, assert_equals
 from scipy.special import gammaln
-
-def score_multinomial(x, probs):
-    assert len(x) == len(probs)
-    term1 = gammaln(x.sum() + 1)
-    term2 = np.sum(gammaln(x + 1))
-    term3 = np.sum(x * np.log(probs))
-    return term1 - term2 + term3
-
-def score_data(Y, alphas):
-    alphas = np.array(alphas)
-    term1 = gammaln(alphas.sum())
-    term2 = -np.sum(gammaln(alphas.sum() + Y.flatten().sum()))
-    term3 = np.sum(gammaln(alphas + Y.sum(axis=0)))
-    term4 = -np.sum(gammaln(alphas))
-    term5 = np.sum(gammaln(Y.sum(axis=1) + 1))
-    term6 = -gammaln(Y + 1).flatten().sum()
-    return term1+term2+term3+term4+term5+term6
 
 def test_dm_cxx():
     K = 4
@@ -54,12 +38,105 @@ def test_dm_cxx():
 
     counts = cxx_s.get_suffstats(0, 0)['counts']
     assert_sequence_equal(counts, list(Y_np.sum(axis=0)))
-    pseudocounts = np.array(counts, dtype=float) + np.array(prior['alphas'], dtype=float)
-    probs = pseudocounts / pseudocounts.sum()
 
-    for i in xrange(Y.shape[0]):
-        _, (cxx_score,) = cxx_s.score_value(Y[i], r)
-        true_score = score_multinomial(Y_np[i], probs)
-        assert_almost_equals(cxx_score, true_score, places=3)
+def test_betabin_equiv():
 
-    assert_almost_equals(score_data(Y_np, prior['alphas']), cxx_s.score_data([], [], r), places=3)
+    def betabin_like(value, alpha, beta, n):
+        """
+        https://github.com/pymc-devs/pymc/blob/a7ab153f2b58d81824a56166747c678d7f421bde/pymc/distributions/discrete.py#L84
+        """
+        return (gammaln(alpha + beta) - gammaln(alpha) - gammaln(beta) +
+                gammaln(n + 1) - gammaln(value + 1) - gammaln(n - value + 1) +
+                gammaln(alpha + value) + gammaln(n + beta - value) - gammaln(beta + alpha + n))
+
+    # this N refers to the number of trials in the binomial distribution
+    N = 10
+
+    # this refers to the dataset size
+    M = 100
+
+    # hyperparams of the beta dist
+    alpha, beta = 1., 2.
+
+    heads = np.random.randint(low=0, high=N+1, size=M)
+    tails = N - heads
+
+    data = np.vstack((heads, tails)).T
+
+    Y = np.array([(y,) for y in data], dtype=[('', np.int, (2,))])
+    view = cxx_numpy_dataview(Y)
+    r = rng()
+    defn = model_definition(Y.shape[0], [dm(2)])
+    prior = {'alphas': [alpha, beta]}
+    s = cxx_initialize(
+        defn,
+        view,
+        r,
+        feature_hps=[prior],
+        assignment=[0]*Y.shape[0])
+
+    assert_equals(s.groups(), [0])
+
+    def all_indices(N):
+        for i, j in it.product(range(0, N+1), repeat=2):
+            if (i + j) == N:
+                yield i, j
+
+    all_data = [(list(ij),) for ij in all_indices(N)]
+
+    Y_test = np.array(all_data, dtype=[('', np.int, (2,))])
+
+    # the actual score is simply a betabin using the updated alpha, beta
+    alpha1, beta1 = np.array([alpha, beta]) + data.sum(axis=0)
+
+    def model_score(Y_value):
+        _, (score,) = s.score_value(Y_value, r)
+        return score
+
+    def test_score(Y_value):
+        score = betabin_like(Y_value[0][0], alpha1, beta1, N)
+        return score
+
+    model_scores = np.array(map(model_score, Y_test))
+    test_scores = np.array(map(test_score, Y_test))
+
+    assert_almost_equals(np.exp(model_scores).sum(), 1., places=2)
+    assert_almost_equals(np.exp(test_scores).sum(), 1., places=2)
+    assert_almost_equals(np.abs(model_scores - test_scores).max(), 0., places=1)
+
+def test_marginal():
+
+    def score_dataset(counts):
+        M, K = counts.shape
+        Y = np.array([(y,) for y in counts], dtype=[('', np.int, (K,))])
+        view = cxx_numpy_dataview(Y)
+        r = rng()
+        defn = model_definition(M, [dm(K)])
+        prior = {'alphas': [1.]*K}
+        s = cxx_initialize(
+            defn,
+            view,
+            r,
+            feature_hps=[prior],
+            assignment=[0]*M)
+        assert_equals(s.groups(), [0])
+        return s.score_data(None, None, r)
+
+    M = 5
+    N = 4
+    K = 2
+
+    def all_indices(N, K):
+        for inds in it.product(range(0, N+1), repeat=K):
+            if sum(inds) == N:
+                yield list(inds)
+
+    def dataset_iter(inds, M):
+        for ptrs in it.product(range(len(inds)), repeat=M):
+            dataset = np.array([inds[p] for p in ptrs])
+            assert dataset.shape == (M, K)
+            score = score_dataset(dataset)
+            yield score
+
+    scores = np.array(list(dataset_iter(list(all_indices(N, K)), M)))
+    assert_almost_equals(np.exp(scores).sum(), 1., places=2)
